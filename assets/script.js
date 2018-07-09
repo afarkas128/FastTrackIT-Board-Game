cd6f82861acf8f053d7f1a36870943bd7b1c4b61
Cristian <moigradancristian@gmail.com>
2018-07-09 18:53:28 +0300

    Roll the dice

--- assets/script.js
+++ assets/script.js
@@ -10,3 +10,15 @@ generatePlayer.addEventListener('click', function() {
 		prompt('What is your name?');
 	}
 });
+function RandomDice() {
+    var Dices = new Array();
+    Dices[0] = "./assets/imgs/dice_1.jpg";
+    Dices[1] = "./assets/imgs/dice_2.jpg";
+    Dices[2] = "./assets/imgs/dice_3.jpg";
+    Dices[3] = "./assets/imgs/dice_4.jpg";
+    Dices[4] = "./assets/imgs/dice_5.jpg";
+    Dices[5] = "./assets/imgs/dice_6.jpg";
+    var random = Math.floor(Math.random() * Dices.length);
+    document.getElementById("numere").innerHTML = "<img src='"
+        + Dices[random] + "' alt='image'></img>";
+}
