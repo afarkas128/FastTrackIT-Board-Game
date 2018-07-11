56dc0568f5ae87b1b942f804ff66f22ab628fac5
Attila Farkas <attila@webgurus.biz>
2018-07-11 10:58:23 +0300

    Map generates on click, css styles

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,6 @@
 var generatePlayer = document.getElementById('generate-player');
+var generateMap = document.getElementById('generate-map');
 var userName = '';
-
 
 generatePlayer.addEventListener('click', function() {
 	userName = prompt('What is your name?');
@@ -10,22 +10,31 @@ generatePlayer.addEventListener('click', function() {
 		prompt('What is your name?');
 	}
 });
+
+/* function that rolls the dice */
 function RandomDice() {
 
     var random = Math.floor(Math.random() * 6) + 1;
     console.log(random);
-    document.getElementById("numere").innerHTML = "<img src='"
-        + `./assets/imgs/dice_${random}.jpg` + "' alt='image'></img>";
+    var numere = document.getElementById("numere").innerHTML = 
+        "<img src='" + `./assets/imgs/dice_${random}.jpg` + "' alt='image'></img>";
     return random;
 }
 
-divs="";
-var i;
-for(i = 1; i <= 30; i++) {
-    divs+= "<div class='row'>" +
-        "<div class='square'>" +
-        "<div class='numberCircle' id="+i+">"+i+"</div></div></div>";
+/* Function that does the mapping for us */
+function generateDivs() {
+    divs = "";
+    // var i;
+    for(var i = 1; i <= 30; i++) {
+        divs+= "<div class='row'>" +
+            "<div class='square'>" +
+            "<div class='numberCircle' id="+ i +">"+ i +"</div></div></div>";
+    }
+    document.getElementById("gol").innerHTML = divs;
 }
-document.getElementById("gol").innerHTML=divs;
 
+/* Calling our map generator function on click */
+generateMap.addEventListener('click', function() {
+    generateDivs();
+});
 
