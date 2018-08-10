f5323ac20c4d7b4de60607a2f25722ded22429ad
Attila Farkas <attilafarkas128@gmail.com>
2018-08-10 17:43:20 +0300

    container width, cleaned js, style

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,12 +1,12 @@
-var generatePlayer = document.getElementById('generate-player');
-var startGame = document.getElementById('start-game');
-var randomDice = document.getElementById('random-dice');
-var userName = '';
-var rollPlayerOne = document.getElementById('roll-player-one');
-var rollPlayerTwo = document.getElementById('roll-player-two');
-var soldat = document.getElementById('gol');
-var player1 = document.createElement("div");
-var player2 = document.createElement("div");
+let generatePlayer = document.getElementById('generate-player');
+let startGame = document.getElementById('start-game');
+let randomDice = document.getElementById('random-dice');
+let userName = '';
+// let rollPlayerOne = document.getElementById('roll-player-one');
+// let rollPlayerTwo = document.getElementById('roll-player-two');
+let soldat = document.getElementById('gol');
+let player1 = document.createElement("div");
+let player2 = document.createElement("div");
 
 
 player1.id="player-unu";
@@ -16,14 +16,14 @@ player2.id="player-doi";
 /* Function that does the mapping for us */
 function generateDivs() {
     divs = "";
-    for(var i = 1; i <= 60; i++) {
+    for(let i = 1; i <= 60; i++) {
         //TODO -
         // rowNumber
         // divs+= '<div class="square row-' + rowNumber + '">' +
         divs += '<div class="square">' + "<div class='numberCircle' id=" + i + ">" + i + "</div></div>";
     }
-    rollPlayerOne.style.display = 'inline-block';
-    rollPlayerTwo.style.display = 'inline-block';
+    // rollPlayerOne.style.display = 'inline-block';
+    // rollPlayerTwo.style.display = 'inline-block';
     // randomDice.style.display = 'inline-block'
     soldat.innerHTML = divs;
     randomDice.style.display = 'inline-block';
@@ -41,8 +41,8 @@ function initialLoad() {
     }).done(function (players){
         console.info('done', players);
 
-        var player1Position = players[0].position;
-        var player2Position = players[1].position;
+        let player1Position = players[0].position;
+        let player2Position = players[1].position;
 
         console.info(player1Position)
         console.info(player2Position)
@@ -52,9 +52,9 @@ function initialLoad() {
     });
 
 }
-
+// function that moves the player dot on the board
 function addPlayerOne(id) {
-    var position = 1;
+    let position = 1;
     if (player1.parentElement) {
         position = parseInt(player1.parentElement.id) + id;
     }
@@ -62,39 +62,33 @@ function addPlayerOne(id) {
 
 }
 function addPlayerTwo(id) {
-    var position = 1;
+    let position = 1;
     if (player2.parentElement) {
         position = parseInt(player2.parentElement.id) + id;
     }
     document.getElementById(position).appendChild(player2);
 }
 
-function removePlayer1(id){
-    var playerUnu= document.getElementById("player-unu");
-    playerUnu.remove();
-}
-function removePlayer2(id){
-    var playerDoi= document.getElementById("player-doi");
-    playerDoi.remove();
-}
+// function removePlayer1(id){
+//     let playerUnu= document.getElementById("player-unu");
+//     playerUnu.remove();
+// }
+// function removePlayer2(id){
+//     let playerDoi= document.getElementById("player-doi");
+//     playerDoi.remove();
+// }
 
 /* function that rolls the dice */
 function rollDice() {
-    var random = Math.floor(Math.random() * 6) + 1;
+    let random = Math.floor(Math.random() * 6) + 1;
     console.log(random);
-    var numere = document.getElementById("numere").innerHTML = `<img src="./images/dice_${random}.jpg" alt="image"></img>`;
+    let numere = document.getElementById("numere").innerHTML = `<img src="public/images/dice_${random}.jpg" alt="image"></img>`;
     return random;
-
 }
 // function randomDice() that roll the dice and moves the player
 randomDice.addEventListener('click', function() {
     addPlayerOne(rollDice());
 });
-// randomDice.addEventListener('click', function() {
-//     addPlayerTwo(rollDice())
-// });
-
-
 
 // updating the players position to the database
 // function updatePlayersPosition() {
@@ -109,5 +103,4 @@ randomDice.addEventListener('click', function() {
 // }
 
 generateDivs(); // generate divs on page load
-
 initialLoad(); // load players positions from database on page load
